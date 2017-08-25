683fe541cae7a8c0fdbd7c47d864408c93137c2c
Clarence <clarence@jollycorp.com>
2017-08-25 15:23:10 +0800

    Revert "shia"
    This reverts commit cb05a1db22e87cccdea4bbb5fd402c5f79771939.

--- TestGit/TestGit/AppDelegate.m
+++ TestGit/TestGit/AppDelegate.m
@@ -17,7 +17,6 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     NSLog(@"enne");
-    NSLog(@"shia");
    
     // Override point for customization after application launch.
     return YES;
