28bb0eb147e8996612450cc1525946be15e74aa9
Clarence <clarence@jollycorp.com>
2017-08-25 14:57:48 +0800

    commit enne

--- TestGit/TestGit/AppDelegate.m
+++ TestGit/TestGit/AppDelegate.m
@@ -16,6 +16,8 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    NSLog(@"enne");
+   
     // Override point for customization after application launch.
     return YES;
 }
