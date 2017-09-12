74f02a071a04948731e7decf85e328f3cbaa9fe2
Clarence <clarence@jollycorp.com>
2017-09-12 10:25:51 +0800

    br3 bug fix

--- TestGit/TestGit/AppDelegate.m
+++ TestGit/TestGit/AppDelegate.m
@@ -29,6 +29,7 @@
     NSLog(@"aa");
     NSLog(@"yes");
     NSLog(@"br2 bug fix");
+    NSLog(@"br3 bug fix");
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
 }
